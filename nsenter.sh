c5b358bdd54dfd66d27b2cc294b13b6aeb126936
kbt48 <hiro.k.0805+github@gmail.com>
2015-09-06 13:59:02 +0900

    Remove unused script.

--- nsenter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-container_id=$1
-if [ -z "$container_id" ]; then
-	echo "Usage: docker_container <container_id>" >&2
-	return 1
-fi
-/usr/bin/nsenter --mount --uts --ipc --net --pid --target $(docker inspect --format '{{.State.Pid}}' $container_id)
-
